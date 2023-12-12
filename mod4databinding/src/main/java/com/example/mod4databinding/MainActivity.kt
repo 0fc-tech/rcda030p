887b985fda66b52db2209898c32b4c43385b8a13
quentinrouet@hotmail.fr <quentinrouet@hotmail.fr>
2023-12-12 14:52:31 +0100

    Mod 4 Databinding

--- mod4databinding/src/main/java/com/example/mod4databinding/MainActivity.kt
+++ mod4databinding/src/main/java/com/example/mod4databinding/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mod4databinding
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mod4databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.user = User(
+        //    "Jean-Michel",
+        //    "Doe",
+        //    "jeanmichel@doe.com"
+        //)
+        binding.user = null//User("Jean-Michel","Doe","jeanmichel@doe.com")
+        binding.button.setOnClickListener {
+            Toast.makeText(this,
+                "${binding.user}",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ mod4databinding/src/main/java/com/example/mod4databinding/User.kt
@@ -0,0 +1,7 @@
+package com.example.mod4databinding
+
+data class User(
+    var prenom :String,
+    var nom :String,
+    var email :String,
+)
